5c5e401573aa7a6610f1b07f0697fa164c522baa
Nick Goede <nick@industriallogic.com>
2021-10-14 14:02:22 -0400

    add describe to test

--- typescript/word.test.ts
+++ typescript/word.test.ts
@@ -1,21 +1,23 @@
-import {Word} from './word'
+import { Word } from './word'
 
-test('isAnagramShouldReturnTrueIfSameWord', () => {
-    const word = new Word('hello');
-    expect(word.isAnagramOf('hello')).toBeTruthy();
-});
+describe('Word', () => {
+    test('isAnagramShouldReturnTrueIfSameWord', () => {
+        const word = new Word('hello');
+        expect(word.isAnagramOf('hello')).toBeTruthy();
+    });
 
-test('isAnagramShouldReturnFalseIfNoLettersInCommon', () => {
-    const word = new Word('abc');
-    expect(word.isAnagramOf('def')).toBeFalsy();
-});
+    test('isAnagramShouldReturnFalseIfNoLettersInCommon', () => {
+        const word = new Word('abc');
+        expect(word.isAnagramOf('def')).toBeFalsy();
+    });
 
-test('isAnagramShouldReturnTrueIfAllLetersAreInCommon', () => {
-    const word = new Word('arrest');
-    expect(word.isAnagramOf('rarest')).toBeTruthy();
-});
+    test('isAnagramShouldReturnTrueIfAllLetersAreInCommon', () => {
+        const word = new Word('arrest');
+        expect(word.isAnagramOf('rarest')).toBeTruthy();
+    });
 
-test('isAnagramShouldReturnTrueOnlyIfSameLength', () => {
-    const word = new Word('arrestt');
-    expect(word.isAnagramOf('rarest')).toBeFalsy();
+    test('isAnagramShouldReturnTrueOnlyIfSameLength', () => {
+        const word = new Word('arrestt');
+        expect(word.isAnagramOf('rarest')).toBeFalsy();
+    });
 });
